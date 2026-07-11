c3e17a5403d6da9c79707cfd74eaf823843e3e96
PRAVEEN-R-2005 <praveenram0808@gmail.com>
2026-07-11 19:22:40 +0530

    commit

--- backend/routes/topCandidateRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { verifyToken, requireRole } = require("../middleware/authMiddleware");
-const { getTopCandidates } = require("../controllers/topCandidateController");
-
-router.get("/", verifyToken, requireRole(["HR", "Admin"]), getTopCandidates);
-
-module.exports = router;
